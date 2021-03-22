b3ee390dac3956ea734e686e3060aa305d73c53f
Mathias Rogey <mathias.rogey@eduvaud.ch>
2021-03-22 10:37:46 +0100

    Implementing the base of json reader

---
+++ P_Thesaurus/AppBusiness/HistoryReader/IHistory.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+using P_Thesaurus.Models.HistoryReader;
+
+namespace P_Thesaurus.AppBusiness.HistoryReader
+{
+    /// <summary>
+    /// IHistory interface
+    /// </summary>
+    /// <typeparam name="T">type of object</typeparam>
+    public interface IHistory<T> : IDisposable
+    {
+        /// <summary>
+        /// History field
+        /// </summary>
+        List<T> History { get; set; }
+
+        /// <summary>
+        /// Reader field
+        /// </summary>
+        JsonReader<List<T>> Reader { get; set; }
+
+        /// <summary>
+        /// AddEntry function
+        /// </summary>
+        /// <param name="entry">entry</param>
+        void AddEntry(T entry);
+    }
+}
---
+++ P_Thesaurus/Models/JSON/JsonReader.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using P_Thesaurus.Models.JSON;
+
+
+namespace P_Thesaurus.Models.HistoryReader
+{
+    /// <summary>
+    /// JsonReader class
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class JsonReader<T> : JsonReaderBase
+    {
+
+    }
+
+
+}
