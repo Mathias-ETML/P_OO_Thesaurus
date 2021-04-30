b0b347d674f208ec95c46f23836041e758cbb7d2
Mathias Rogey <mathias.rogey@eduvaud.ch>
2021-04-30 16:58:51 +0200

    Controller factory done

--- P_Thesaurus/Controllers/ControllerFactory.cs
+++ P_Thesaurus/Controllers/ControllerFactory.cs
@@ -6,7 +6,6 @@
  * Factory utilisée par le MainController pour instancier les controllers dont il a besoin
  */
 using System;
-using P_Thesaurus.Controllers;
 using P_Thesaurus.AppBusiness.EnumsAndStructs;
 
 namespace P_Thesaurus.Controllers
@@ -18,7 +17,6 @@ namespace P_Thesaurus.Controllers
     {
         #region Variables
         private bool _disposedValue;
-
         #endregion
 
         #region Public Methods
@@ -30,40 +28,56 @@ namespace P_Thesaurus.Controllers
 
         }
 
+        /// <summary>
+        /// GetController function
+        /// </summary>
+        /// <param name="type">type of controller</param>
+        /// <returns>the controller that fit the type</returns>
         public IController GetController(ControllerType type)
         {
-            return null;
+            switch (type)
+            {
+                case ControllerType.Launching:
+                    return new LaunchController();
+
+                case ControllerType.Ftp:
+                    return new FtpController();
+
+                case ControllerType.Folder:
+                    return new FolderController();
+
+                case ControllerType.Web:
+                    return new WebController();
+
+                default:
+                    throw new ArgumentException("Please include a valid type of controller");
+            }
         }
         #endregion
 
+        /// <summary>
+        /// Dispose function
+        /// </summary>
+        /// <param name="disposing">disposing</param>
         #region Dispose Model
-
-
         protected virtual void Dispose(bool disposing)
         {
             if (!_disposedValue)
             {
                 if (disposing)
                 {
-                    // TODO: supprimer l'état managé (objets managés)
+
                 }
 
-                // TODO: libérer les ressources non managées (objets non managés) et substituer le finaliseur
-                // TODO: affecter aux grands champs une valeur null
                 _disposedValue = true;
             }
         }
 
-        // // TODO: substituer le finaliseur uniquement si 'Dispose(bool disposing)' a du code pour libérer les ressources non managées
-        // ~ControllerFactory()
-        // {
-        //     // Ne changez pas ce code. Placez le code de nettoyage dans la méthode 'Dispose(bool disposing)'
-        //     Dispose(disposing: false);
-        // }
-
+        /// <summary>
+        /// Dispose function
+        /// </summary>
         public void Dispose()
         {
-            // Ne changez pas ce code. Placez le code de nettoyage dans la méthode 'Dispose(bool disposing)'
             Dispose(disposing: true);
             GC.SuppressFinalize(this);
         }
